33e87aa36f98299fd4bfe8b7678bd2f2dfb4de09
Kimthina Ly <kimthinaly7@gmail.com>
2026-08-06 17:56:03 +0700

    feat(logic+ui):[assignment2] ui for shape input/output, refractor logic code

---
+++ assignment2/shapeCalLogic.js
@@ -0,0 +1,55 @@
+class Triangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  
+  area() {
+    return (this.width * this.height) / 2;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  area(){
+    return this.width * this.height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  area() {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+
+const geometry = {
+
+  //helper function
+  _createObject: function(shape, dimensions) { 
+    const { width, height, radius } = dimensions;
+    switch(shape){
+      case "triangle":
+        return new Triangle(width, height);
+      case "rectangle":
+        return new Rectangle(width, height);
+      case "circle":
+        return new Circle(radius);
+      default:
+        return null;
+    }
+  },
+
+  calculateArea(shape, dimensions) {
+    const object = this._createObject(shape, dimensions);
+    return object ? object.area() : 0;
+  }
+}
